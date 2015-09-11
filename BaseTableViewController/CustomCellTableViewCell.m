0966d21ab2c3d39ba2d93cd6616ce436d885f02f
Praveen Madanu <praveenjmadanu@gmail.com>
2015-09-11 19:22:44 +0530

    added

---
+++ BaseTableViewController/CustomCellTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomCellTableViewCell.m
+//  BaseTableViewController
+//
+//  Created by praveen madanu on 28/08/15.
+//  Copyright (c) 2015 Primesoft Solutions Inc. All rights reserved.
+//
+
+#import "CustomCellTableViewCell.h"
+
+@implementation CustomCellTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
